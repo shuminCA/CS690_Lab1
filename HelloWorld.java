59debcaf13727c7c63c42c515dcfaedd08685aa2
Bing Liang <ice@Bings-MacBook-Pro.local>
2023-08-25 16:32:50 -0700

    Resolved comments

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+   	System.out.println("C++ is the best language in the world");
     }
 }
